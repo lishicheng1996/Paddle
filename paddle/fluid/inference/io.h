a87b3d06292435d6645eedb847190245b4c8c85e
lishicheng1996 <847223866@qq.com>
2023-12-18 12:15:14 +0800

    fix bug

--- paddle/fluid/inference/io.h
+++ paddle/fluid/inference/io.h
@@ -32,6 +32,7 @@ void LoadPersistables(framework::Executor* executor,
                       framework::Scope* scope,
                       const framework::ProgramDesc& main_program,
                       const std::string& dirname,
+                      const std::string& param_filename,
                       bool model_from_memory = false,
                       const std::string& parse = "");
 
