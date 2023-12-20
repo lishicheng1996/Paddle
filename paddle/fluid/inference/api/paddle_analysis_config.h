da2565c567eb805463b6126f51e5ebc9bd04b570
lishicheng1996 <847223866@qq.com>
2023-12-20 17:24:43 +0800

    fix a bug

--- paddle/fluid/inference/api/paddle_analysis_config.h
+++ paddle/fluid/inference/api/paddle_analysis_config.h
@@ -865,7 +865,7 @@ struct PD_INFER_DECL AnalysisConfig {
   }
 
 #ifdef PADDLE_WITH_FASTDEPLOY_AUTH
-  void SetFDProductName(string name = "safety-query-safety") {fastdeploy_ep_product_name_ = name};
+  void SetFDProductName(std::string name = "safety-query-safety") {fastdeploy_ep_product_name_ = name};
   string get_fd_product_name() {return fastdeploy_ep_product_name_};
 #endif
 
